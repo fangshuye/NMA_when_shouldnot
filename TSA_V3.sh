947d85d8de7f22a080e6617af86aba2eb864216c
Fangshu Ye <56810803+fangshuye@users.noreply.github.com>
2022-11-09 09:38:55 -0600

    Add files via upload

---
+++ TSA_V3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --time=2:00:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=16   # 16 processor core(s) per node 
+#SBATCH --error=job.%J.err 
+#SBATCH --output=job.%J.out 
+#SBATCH --mail-user=fangshu.stat@gmail.com   # email address
+#SBATCH --mail-type=END
+module load r/4.0.4-py3-4khjixy
+Rscript TSA_alpha_V3.R --r $1 > NMA_$SLURM_JOBID.Rout
+
+
